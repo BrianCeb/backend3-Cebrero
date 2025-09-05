a5dfeb117181caa17b25cca4fc0faef0b94c5847
brian cebrero <briancebrero@gmail.com>
2025-09-05 15:34:12 -0300

    docker

--- src/app.js
+++ src/app.js
@@ -10,6 +10,11 @@ import sessionsRouter from './routes/sessions.router.js';
 import mocksRouter from './routes/mocks.router.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 
 const app = express();
@@ -18,7 +23,8 @@ const PORT = process.env.PORT || 8080;
 console.log('MONGO_URL presente?', Boolean(process.env.MONGO_URL));
 
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URL)
+mongoose
+    .connect(process.env.MONGO_URL)
     .then(() => console.log(' MongoDB conectado'))
     .catch(err => console.error(' Error de conexión a MongoDB:', err));
 
@@ -35,6 +41,27 @@ app.use('/api/adoptions', adoptionsRouter);
 app.use('/api/sessions', sessionsRouter);
 app.use('/api/mocks', mocksRouter);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.1',
+        info: {
+            title: 'Adoptme API',
+            description: 'Documentación del módulo Users',
+            version: '1.0.0'
+        }
+    },
+    apis: [path.join(process.cwd(), 'src', 'docs', '**', '*.yaml')]
+};
+
+const specs = swaggerJsdoc(swaggerOptions);
+app.use('/apidocs', swaggerUi.serve, swaggerUi.setup(specs));
+
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+}
+export default app;
